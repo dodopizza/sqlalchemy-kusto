83a3056c1eb1b70c94a1d97d48c4a96aec1d9751
k.tomak <k.tomak@dodopizza.com>
2021-05-11 15:47:43 +0300

    feat: kusto db api cursor

---
+++ sqlalchemy_kusto/dbapi_cursor.py
@@ -0,0 +1,104 @@
+from sqlalchemy_kusto.kusto_dbapi import check_closed, check_result, apply_parameters
+from azure.kusto.data import KustoClient, ClientRequestProperties
+from typing import Optional
+
+
+class Cursor(object):
+    """Connection cursor."""
+
+    def __init__(
+        self,
+        kusto_client: KustoClient = None,
+        database: str = None,
+        properties: Optional[ClientRequestProperties] = None,
+    ):
+        self.kusto_client = kusto_client
+        self.database = database
+        self.properties = properties
+        self.closed = False
+        self.description = None
+        self._results = None
+
+    @property
+    @check_result
+    @check_closed
+    def rowcount(self):
+        # consume the iterator
+        results = list(self._results)
+        n = len(results)
+        self._results = iter(results)
+        return n
+
+    @check_closed
+    def close(self):
+        """Close the cursor."""
+        self.closed = True
+
+    @check_closed
+    def execute(self, operation, parameters=None):
+        query = apply_parameters(operation, parameters)
+        self._results = self.kusto_client.execute_query(self.database, query, self.properties)
+
+        return self
+
+    @check_closed
+    def executemany(self, operation, seq_of_parameters=None):
+        raise NotImplementedError(
+            "`executemany` is not supported, use `execute` instead"
+        )
+
+    @check_result
+    @check_closed
+    def fetchone(self):
+        raise NotImplementedError(
+            "`fetchone` is not supported, use `execute` instead"
+        )
+
+    @check_result
+    @check_closed
+    def fetchmany(self, size=None):
+        raise NotImplementedError(
+            "`fetchmany` is not supported, use `execute` instead"
+        )
+
+
+    @check_result
+    @check_closed
+    def fetchall(self):
+        """
+        Fetch all (remaining) rows of a query result, returning them as a
+        sequence of sequences (e.g. a list of tuples). Note that the cursor's
+        arraysize attribute can affect the performance of this operation.
+        """
+        return list(self._results)
+
+    @check_closed
+    def setinputsizes(self, sizes):
+        # not supported
+        pass
+
+    @check_closed
+    def setoutputsizes(self, sizes):
+        # not supported
+        pass
+
+    @check_closed
+    def __iter__(self):
+        return self
+
+    @check_closed
+    def __next__(self):
+        return next(self._results)
+
+    next = __next__
+
+
+def check_result(f):
+    """Decorator that checks if the cursor has results from `execute`."""
+
+    def g(self, *args, **kwargs):
+        if self._results is None:
+            raise Exception("Called before `execute`")
+        return f(self, *args, **kwargs)
+
+    return g
