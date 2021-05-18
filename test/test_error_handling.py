9565af2265f5b42fd031b2cead6c06f4e0fd29ad
Eugene Bikkinin <xnegxneg@gmail.com>
2021-05-18 14:02:26 +0300

    fix: error handling test.

--- test/test_error_handling.py
+++ test/test_error_handling.py
@@ -1,17 +1,18 @@
-from test.conftest import KUSTO_ALCHEMY_URL, DATABASE, AZURE_AD_CLIENT_ID, AZURE_AD_CLIENT_SECRET, AZURE_AD_TENANT_ID
+from test.conftest import KUSTO_ALCHEMY_URL, DATABASE, AZURE_AD_CLIENT_ID, AZURE_AD_CLIENT_SECRET
 import pytest
+import sqlalchemy
 from sqlalchemy import create_engine
-
-import sqlalchemy_kusto.errors
 
 
 def test_query_error():
+    wrong_tenant_id = "wrong_tenant_id"
     engine = create_engine(
         f"{KUSTO_ALCHEMY_URL}/{DATABASE}?"
         f"msi=False&azure_ad_client_id={AZURE_AD_CLIENT_ID}&"
         f"azure_ad_client_secret={AZURE_AD_CLIENT_SECRET}&"
-        f"azure_ad_tenant_id={AZURE_AD_TENANT_ID}tt"
+        f"azure_ad_tenant_id={wrong_tenant_id}"
     )
     engine.connect()
-    with pytest.raises(sqlalchemy_kusto.errors.OperationalError):
-        engine.execute("select limit * from MaterialTransferStream")
+
+    with pytest.raises(sqlalchemy.exc.OperationalError):
+        engine.execute("select top 5 * from MaterialTransferStream")
