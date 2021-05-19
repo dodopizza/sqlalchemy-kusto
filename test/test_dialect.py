50dd76d1b4db5f50d96ec066c924e6e3bb67d6d5
Eugene Bikkinin <xnegxneg@gmail.com>
2021-05-19 09:41:10 +0300

    fix: add compilator test.

--- test/test_dialect.py
+++ test/test_dialect.py
@@ -1,6 +1,8 @@
+from sqlalchemy.sql.selectable import TextAsFrom
+
 from test.conftest import KUSTO_ALCHEMY_URL, DATABASE, AZURE_AD_CLIENT_ID, AZURE_AD_CLIENT_SECRET, AZURE_AD_TENANT_ID
 from sqlalchemy import Table, Column, String, MetaData, create_engine
-
+import sqlalchemy as sa
 
 def test_limit():
     metadata = MetaData()
@@ -22,3 +24,25 @@ def test_limit():
     result = engine.execute(query)
     result_length = len(result.fetchall())
     assert result_length == 5
+
+def test_compilator():
+    statement_str = "MaterialTransferStream | take 10"
+    # statement_str = "Select top 100 * from MaterialTransferStream"
+
+    engine = create_engine(
+        f"{KUSTO_ALCHEMY_URL}/{DATABASE}?"
+        f"msi=False&azure_ad_client_id={AZURE_AD_CLIENT_ID}&"
+        f"azure_ad_client_secret={AZURE_AD_CLIENT_SECRET}&"
+        f"azure_ad_tenant_id={AZURE_AD_TENANT_ID}"
+    )
+    engine.connect()
+
+    stmt = TextAsFrom(sa.text(statement_str), []).alias("virtual_table")
+    print(str(stmt))
+    query = sa.select(stmt, "*")
+    query = query.select_from(stmt)
+    query = query.limit(10)
+    # query = stmt.select()
+    engine.execute(query)
+    print("========")
+    print(str(query))
