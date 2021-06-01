27c90c45980bb6754a92d8defd8e5b38a210ce12
Eugene Bikkinin <xnegxneg@gmail.com>
2021-06-01 15:50:29 +0300

    add assertion to test_compiler for Kql

--- test/test_dialect_kql.py
+++ test/test_dialect_kql.py
@@ -13,7 +13,6 @@ from sqlalchemy.sql.selectable import TextAsFrom
 
 def test_compiler():
     statement_str = "MaterialTransferStream | take 10"
-    # statement_str = "Select top 100 * from MaterialTransferStream"
 
     engine = create_engine(
         f"{KUSTO_KQL_ALCHEMY_URL}/{DATABASE}?"
@@ -21,12 +20,17 @@ def test_compiler():
         f"azure_ad_client_secret={AZURE_AD_CLIENT_SECRET}&"
         f"azure_ad_tenant_id={AZURE_AD_TENANT_ID}"
     )
-    engine.connect()
 
     stmt = TextAsFrom(sa.text(statement_str), []).alias("virtual_table")
     query = sa.select(from_obj=stmt, columns=[column("UnitId").label("uId")])
     query = query.select_from(stmt)
     query = query.limit(10)
-    print("========")
-    print(str(query))
-    print("========")
+
+    query_compiled = str(query.compile(engine))
+    query_expected = [
+        "let virtual_table = (MaterialTransferStream | take 10);",
+        "virtual_table",
+        "| project uId = uId",
+        "| take %(param_1)s"]
+
+    assert query_compiled == "\n".join(query_expected)
