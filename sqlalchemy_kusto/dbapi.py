65d7e594a10d82ae1b3c9335ca0038088d15e99f
k.tomak <k.tomak@dodopizza.com>
2021-12-27 11:45:01 +0300

    refactor: refactor Cursor

--- sqlalchemy_kusto/dbapi.py
+++ sqlalchemy_kusto/dbapi.py
@@ -1,8 +1,12 @@
-from typing import List
 
 from azure.kusto.data import KustoClient, KustoConnectionStringBuilder, ClientRequestProperties
-from sqlalchemy_kusto.dbapi_cursor import Cursor
-from sqlalchemy_kusto.utils import check_closed
+from collections import namedtuple
+from typing import Optional, List
+from azure.kusto.data._models import KustoResultColumn
+from azure.kusto.data import KustoClient, ClientRequestProperties
+from azure.kusto.data.exceptions import KustoServiceError, KustoAuthenticationError
+from sqlalchemy_kusto import errors
+from sqlalchemy_kusto.utils import check_closed, check_result
 
 
 def connect(
@@ -24,6 +28,7 @@ def connect(
         azure_ad_client_secret,
         azure_ad_tenant_id,
     )
+
 
 class Connection:
     """Connection to Kusto cluster."""
@@ -96,3 +101,175 @@ class Connection:
 
     def __exit__(self, *exc):
         self.close()
+
+
+CursorDescriptionRow = namedtuple(
+    "CursorDescriptionRow",
+    ["name", "type", "display_size", "internal_size", "precision", "scale", "null_ok"],
+)
+
+
+class Cursor:
+    """Connection cursor."""
+
+    def __init__(
+        self,
+        kusto_client: KustoClient,
+        database: str,
+        properties: Optional[ClientRequestProperties] = None,
+    ):
+        self.kusto_client = kusto_client
+        self.database = database
+        self.closed = False
+        self.description = None
+        self._results = None
+        self.current_item_index = 0
+        self.properties = properties if properties is not None else ClientRequestProperties()
+
+    @property
+    @check_result
+    @check_closed
+    def rowcount(self) -> int:
+        """Count the number of rows on a result."""
+        # consume the iterator
+        results = list(self._results)
+        return len(results)
+
+    @check_closed
+    def close(self):
+        """Close the cursor."""
+        self.closed = True
+
+    @check_closed
+    def execute(self, operation, parameters=None) -> "Cursor":
+        """Execute query. Supports only SELECT statements."""
+        if operation.lower().startswith("select"):
+            self.properties.set_option("query_language", "sql")
+        else:
+            self.properties.set_option("query_language", "kql")
+
+        query = Cursor._apply_parameters(operation, parameters)
+        query = query.rstrip()
+        try:
+            server_response = self.kusto_client.execute(self.database, query, self.properties)
+        except KustoServiceError as kusto_error:
+            raise errors.DatabaseError(str(kusto_error))
+        except KustoAuthenticationError as context_error:
+            raise errors.OperationalError(str(context_error))
+
+        rows = []
+        for row in server_response.primary_results[0]:
+            rows.append(tuple(row.to_list()))
+        self._results = rows
+        self.description = self._get_description_from_columns(server_response.primary_results[0].columns)
+        return self
+
+    @check_closed
+    def executemany(self, operation, seq_of_parameters=None):
+        """Not supported"""
+        raise NotImplementedError("`executemany` is not supported, use `execute` instead")
+
+    @check_result
+    @check_closed
+    def fetchone(self):
+        """
+        Fetch the next row of a query result set, returning a single sequence,
+        or `None` when no more data is available.
+        """
+        if self.rowcount > self.current_item_index:
+            item = self._results[self.current_item_index]
+            self.current_item_index += 1
+            return item
+
+        return None
+
+    @check_result
+    @check_closed
+    def fetchmany(self, size: int = None):
+        """
+        Fetch the next set of rows of a query result, returning a sequence of
+        sequences (e.g. a list of tuples). An empty sequence is returned when
+        no more rows are available.
+        """
+        if size:
+            items = self._results[self.current_item_index: self.current_item_index + size]
+            self.current_item_index += size
+            return items
+
+        return self._results
+
+    @check_result
+    @check_closed
+    def fetchall(self):
+        """
+        Fetch all (remaining) rows of a query result, returning them as a
+        sequence of sequences (e.g. a list of tuples). Note that the cursor's
+        arraysize attribute can affect the performance of this operation.
+        """
+        return list(self._results)
+
+    @check_closed
+    def setinputsizes(self, sizes):
+        """Not supported"""
+        pass
+
+    @check_closed
+    def setoutputsizes(self, sizes):
+        """Not supported"""
+        pass
+
+    @staticmethod
+    def _get_description_from_columns(columns: List[KustoResultColumn]) -> List[CursorDescriptionRow]:
+        """Get CursorDescriptionRow for Kusto columns"""
+        return [
+            CursorDescriptionRow(
+                name=column.column_name,
+                type=column.column_type,
+                display_size=None,
+                internal_size=None,
+                precision=None,
+                scale=None,
+                null_ok=True,
+            )
+            for column in columns
+        ]
+
+    @check_closed
+    def __iter__(self):
+        return self
+
+    @check_closed
+    def __next__(self):
+        return next(self._results)
+
+    next = __next__
+
+    @staticmethod
+    def _apply_parameters(operation, parameters) -> str:
+        """Apply parameters to operation string"""
+        if not parameters:
+            return operation
+
+        escaped_parameters = {key: Cursor._escape(value) for key, value in parameters.items()}
+        return operation % escaped_parameters
+
+    @staticmethod
+    def _escape(value) -> str:
+        """
+        Escape the parameter value.
+
+        Note that bool is a subclass of int so order of statements matter.
+        """
+
+        if value == "*":
+            return value
+        if isinstance(value, str):
+            return "'{}'".format(value.replace("'", "''"))
+        if isinstance(value, bool):
+            return "TRUE" if value else "FALSE"
+        if isinstance(value, (int, float)):
+            return value
+        if isinstance(value, (list, tuple)):
+            return ", ".join(Cursor._escape(element) for element in value)
+
+        return value
